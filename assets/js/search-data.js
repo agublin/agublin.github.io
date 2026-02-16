1a990380e9b02b7db6a39f3d9eb64fe580643d2e
agublin <arthur@gublin.com>
2026-02-16 09:29:36 +0000

    Deploying to gh-pages from @ agublin/agublin.github.io@1da8a3b63a83fb599dac6c5a84aaaf0a56eccc70 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-start-of-my-doctorate-laser-induced-breakdowmn-spectroscopy-withotu-calibration-for-quantittative-isotopic-analysis-of-uranium",
+          title: 'Start of my doctorate : Laser Induced Breakdowmn Spectroscopy withotu calibration for quantittative...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
